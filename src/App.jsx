157d6312365daa9c58b6c3284e439df83ea6cec6
narodstream <vlad_vorobyev@mail.ru>
2026-04-09 13:36:36 +0300

    LESSON06

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+//import { TEST1, TEST2 } from './test';
+import './index.css';
+import Dog from './dog.jpg';
+
+import { hello2 } from './myFolder';
+import { hello } from './myFolder/hello';
+
+console.log(hello2, hello);
 
 function App() {
   return (
     <>
       Hello
+      <div className='hello'>
+      <img src={Dog} />
+    </div>
     </>
   )
 }
---
+++ src/test.jsx
@@ -0,0 +1,22 @@
+export const TEST1 = 5;
+export const TEST2 = 6;
+
+// export const Test3 = () => {
+
+// };
+
+// export function Test4() {
+
+// }
+
+// export class Test5 {
+
+// }
+
+// export { TEST1, TEST2 };
+
+const TEST5 = 7;
+
+export default TEST5;
+
+// import / export 
